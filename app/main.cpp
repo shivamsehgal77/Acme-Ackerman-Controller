cda48da6846e1fa0af33dd36e8ac6f072461c9cd
shivamsehgal77 <112571645+shivamsehgal77@users.noreply.github.com>
2023-10-17 17:02:44 -0400

    1.) created header files PID.hpp and ackerman.hpp. 2.) created stubs for the following files mentioned above.

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,8 @@
-#include "lib.hpp"
-#include "lib1.hpp"
-#include "lib2.hpp"
+#include "PID.hpp"
+#include "ackerman.hpp"
+#include <iostream>
 
 int main() {
-  dummy();
-  my_function1(30);
-  my_function2(30.3);
-  return 0;
+return 0;
 }
+
