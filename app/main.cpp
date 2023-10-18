7b2811979026c414f155c09d5ae59a2e8a9822b3
Darshit Desai <36150235+darshit-desai@users.noreply.github.com>
2023-10-18 18:23:01 -0400

    Update main.cpp
    Phase 0 author name and roles updates

--- app/main.cpp
+++ app/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file ackerman.cpp
+ * @author Phase 0 - Shivam Sehgal (ssehgal7@umd.edu) - Driver, Patrik Pordi (ppordi@umd.edu) - Navigator, Darshit Desai (darshit@umd.edu) - Code designer
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-17
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include "lib.hpp"
 #include "lib1.hpp"
 #include "lib2.hpp"
