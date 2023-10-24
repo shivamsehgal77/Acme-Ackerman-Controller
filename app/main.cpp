c0ac7c816c83027f598e1544949f8e1400feb708
Shivam Sehgal <112571645+shivamsehgal77@users.noreply.github.com>
2023-10-23 23:34:59 -0400

    Revert "Dev iter1 branch stub and test commits and changes"

--- app/main.cpp
+++ app/main.cpp
@@ -1,20 +1,19 @@
 /**
  * @file main.cpp
- * @author Phase 0 - Shivam Sehgal (ssehgal7@umd.edu) - Driver, Patrik Pordi
- * (ppordi@umd.edu) - Navigator, Darshit Desai (darshit@umd.edu) - Code designer
- * @brief Main file for running the PID and Ackerman Steering Model
+ * @author Phase 0 - Shivam Sehgal (ssehgal7@umd.edu) - Driver, Patrik Pordi (ppordi@umd.edu) - Navigator, Darshit Desai (darshit@umd.edu) - Code designer
+ * @brief 
  * @version 0.1
  * @date 2023-10-17
- *
- * @copyright Copyright (c) 2023 Darshit Desai, Patrik Pordi, Shivam Sehgal
- * This code is licensed under the MIT License. Please see the
- * accompanying LICENSE file for the full text of the license.
- *
+ * 
+ * @copyright Copyright (c) 2023
+ * 
  */
 
-#include <iostream>
+#include "lib.hpp"
+#include "lib1.hpp"
+#include "lib2.hpp"
 
-#include "../include/PID.hpp"
-#include "../include/ackerman.hpp"
+int main() {
+return 0;
+}
 
-int main() { return 0; }
